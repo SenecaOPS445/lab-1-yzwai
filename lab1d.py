b105f7008e49724565929b8c9aa89f7d1f67f2de
Ye Zin Wai <yzwai@myseneca.ca>
2024-05-14 21:39:54 -0400

    Addad lab1d

---
+++ lab1d.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# Define the variables
+x = 10
+y = 2
+z = 5
+
+# Calculate the result
+sum = x + y * z
+
+# Convert the integers to strings
+xstr = str(x)
+ystr = str(y)
+zstr = str(z)
+sumstr = str(sum)
+
+# Print the sum
+print(xstr + " + " + ystr + " * " + zstr + " = " + sumstr)
+
